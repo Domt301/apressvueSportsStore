2c57730af8be0fd8dcbdc68bdf174ba55c9777e0
Leonardo Trimarchi <Leonardod.Trimarchi@gmail.com>
2018-12-08 21:06:13 -0500

    Chapter 5 Follow along completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <store />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import Store from "./components/Store";
+    import { mapActions } from "vuex";
+    export default {
+        name: 'app',
+        components: { Store },
+        methods: {
+            ...mapActions(["getData"])
+        },
+        created() {
+            this.getData();
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
