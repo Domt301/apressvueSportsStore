48f5ed960d14f27f9bfc67d061697b389d4bc4af
Leonardo Trimarchi <Leonardod.Trimarchi@gmail.com>
2018-12-25 18:36:56 -0500

    made chapter 8 changes and added dockerfile

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,17 @@ import Store from "../components/Store";
 import ShoppingCart from "../components/ShoppingCart";
 import Checkout from "../components/Checkout";
 import OrderThanks from "../components/OrderThanks";
-import Authentication from "../components/admin/Authentication";
-import Admin from "../components/admin/Admin";
-import ProductAdmin from "../components/admin/ProductAdmin";
-import OrderAdmin from "../components/admin/OrderAdmin";
+
+const Authentication = () => 
+    import(/* webpackChunkName: "admin" */ "../components/admin/Authentication");
+const Admin = () => 
+    import(/* webpackChunkName: "admin" */ "../components/admin/Admin");
+const ProductAdmin = () => 
+    import(/* webpackChunkName: "admin" */ "../components/admin/ProductAdmin");
+const OrderAdmin = () =>  
+    import(/* webpackChunkName: "admin" */ "../components/admin/OrderAdmin");
+const ProductEditor = () => 
+    import(/* webpackChunkName: "admin" */ "../components/admin/ProductEditor");
 
 import dataStore from "../store";
 
@@ -30,6 +37,8 @@ export default new VueRouter({
                 }
             },
             children: [
+                { path: "products/:op(create|edit)/:id(\\d+)?", 
+                      component: ProductEditor },
                 { path: "products", component: ProductAdmin },
                 { path: "orders", component: OrderAdmin },
                 { path: "", redirect: "/admin/products"}
